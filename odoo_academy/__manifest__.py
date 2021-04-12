948c103f7bf4fb9e140fa8b10fb5be847db3a11b
DF <david.reyes@oficinastecnologicas.com>
2021-04-12 16:57:54 +0000

    [ADD] Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Odoo Academy",
+    "summary": """Academy app to manage Training""",
+    "description": """
+        Academy app to manage Training:
+        - Courses
+        - Sessions
+        - Attendance
+    """,
+    "author": "DF",
+    "website": "https://www.odoo.com",
+    "category": "Training",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [
+        "views/course_views.xml",
+        "views/academy_menuitems.xml",
+        "security/academy_security.xml",
+        "security/ir.model.access.csv"
+    ],
+    "demo": [
+        "demo/academy_demo.xml",
+    ]
+}
