2fedef549ca87d09dd80eeff0d65ba1978eb0416
DF <david.reyes@oficinastecnologicas.com>
2021-04-14 18:56:45 +0000

    [ADD] Inheritance

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -13,13 +13,14 @@
     "website": "https://www.odoo.com",
     "category": "Training",
     "version": "0.1",
-    "depends": ["base"],
+    "depends": ["sale"],
     "data": [
         "views/course_views.xml",
         "views/session_views.xml",
         "views/academy_menuitems.xml",
         "security/academy_security.xml",
-        "security/ir.model.access.csv"
+        "security/ir.model.access.csv",
+        "views/sale_views_inherit.xml"
     ],
     "demo": [
         "demo/academy_demo.xml",
